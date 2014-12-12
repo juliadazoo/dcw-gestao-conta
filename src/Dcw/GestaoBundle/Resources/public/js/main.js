0bde2969e444f7fb3569a1b75c1222fb28f585a8
Hugo Augusto Freitas do Carmo <hugo.carmo@aker.com.br>
2014-12-12 17:13:13 -0200

    [js] Common project config
    - Project main javascript rules;

---
+++ src/Dcw/GestaoBundle/Resources/public/js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#entity-table").dataTable({
+        "language": {
+            "url": "/gestao-contas/web/bundles/dcwgestao/json/datatables-pt_BR.json"
+        }
+    });
+});
